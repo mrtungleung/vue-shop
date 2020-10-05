e978fd60e16161c59c66fbd8d61674b7b994ac23
Liang Tong <lt81192@126.com>
2020-10-05 15:40:30 +0800

    完成了登录功能

--- src/main.js
+++ src/main.js
@@ -3,6 +3,12 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './plugins/element.js'
+import './assets/css/global.css'
+import './assets/iconfont/font_2113332_1awkd9szohb/iconfont.css'
+import axios from 'axios'
+// 配置请求根路径
+axios.defaults.baseURL = 'https://www.liulongbin.top:8888/api/private/v1/'
+Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
 
