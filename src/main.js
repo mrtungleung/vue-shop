0c33bf54449d7dd790e68c3ce1bdb7386c1d973d
liangt15 <liangt15@chinaunicom.cn>
2020-10-14 11:40:00 +0800

    增加Home主页

--- src/main.js
+++ src/main.js
@@ -1,16 +1,25 @@
 import Vue from 'vue'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 import './plugins/element.js'
 import './assets/css/global.css'
-import './assets/iconfont/font_2113332_1awkd9szohb/iconfont.css'
+import './assets/iconfont/font_2113332_763e8nme5up/iconfont.css'
 import axios from 'axios'
 // 配置请求根路径
 axios.defaults.baseURL = 'https://www.liulongbin.top:8888/api/private/v1/'
+axios.interceptors.request.use(config => {
+  config.headers.Authorization = window.sessionStorage.getItem('token')
+  // 最后必须 return config
+  return config
+})
 Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
+
+Vue.use(ElementUI)
 
 new Vue({
   router,
