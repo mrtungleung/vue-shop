7d9c51ff7d136ba0b637c63b4e14a4573423704c
liangt15 <liangt15@chinaunicom.cn>
2020-10-30 10:55:42 +0800

    完成角色编辑功能

--- src/components/power/Roles.vue
+++ src/components/power/Roles.vue
@@ -49,7 +49,7 @@
         <el-table-column label="角色描述" prop="roleDesc"></el-table-column>
         <el-table-column label="操作">
           <template slot-scope="scope">
-            <el-button size="mini" type="primary" icon="el-icon-edit">编辑</el-button>
+            <el-button size="mini" type="primary" icon="el-icon-edit" @click="showEditRoleDialog(scope.row.id)">编辑</el-button>
             <el-button size="mini" type="danger" icon="el-icon-delete">删除</el-button>
             <el-button size="mini" type="warning" icon="el-icon-setting" @click="showSetRightDialog(scope.row)">分配权限</el-button>
           </template>
@@ -76,7 +76,25 @@
         <el-button type="primary" @click="addRole">确 定</el-button>
       </span>
     </el-dialog>
-
+    <!-- 修改角色的对话框 -->
+    <el-dialog
+      title="修改角色"
+      :visible.sync="editRoleDialogVisible"
+      width="50%"
+      >
+      <el-form :model="editForm" :rules="editFormRules" ref="editFormRef" label-width="80px">
+        <el-form-item label="角色名称" prop="roleName">
+          <el-input v-model="editForm.roleName"></el-input>
+        </el-form-item>
+        <el-form-item label="角色描述" prop="roleDesc">
+          <el-input v-model="editForm.roleDesc"></el-input>
+        </el-form-item>
+      </el-form>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="editRoleDialogVisible = false">取 消</el-button>
+        <el-button type="primary" @click="editRoleInfo">确 定</el-button>
+      </span>
+    </el-dialog>
     <!-- 分配权限的对话框 -->
     <el-dialog
       title="提示"
@@ -126,6 +144,21 @@ export default {
       },
       // 添加表单的验证规则对象
       addFormRules: {
+        roleName: [
+          { required: true, message: '请输入角色名称', trigger: 'blur' },
+          { min: 3, max: 10, message: '用户名的长度在3~10个字符之间', trigger: 'blur' }
+        ],
+        roleDesc: [
+          { required: true, message: '请输角色描述', trigger: 'blur' },
+          { min: 3, max: 10, message: '用户名的长度在3~10个字符之间', trigger: 'blur' }
+        ]
+      },
+      // 控制修改角色的对话框的显示与隐藏
+      editRoleDialogVisible: false,
+      // 查询到的用户角色对象
+      editForm: {},
+      // 修改角色表单的验证规则对象
+      editFormRules: {
         roleName: [
           { required: true, message: '请输入角色名称', trigger: 'blur' },
           { min: 3, max: 10, message: '用户名的长度在3~10个字符之间', trigger: 'blur' }
@@ -236,6 +269,36 @@ export default {
     // 关闭添加角色对话框
     addRoleDialogClosed () {
       this.$refs.addFormRef.resetFields()
+    },
+    // 控制修改角色对话框的显示与隐藏
+    async showEditRoleDialog (id) {
+      const { data: res } = await this.$http.get('roles/' + id)
+      if (res.meta.status !== 200) {
+        return this.$message.error('查询角色信息失败！')
+      }
+      this.editForm = res.data
+      this.editRoleDialogVisible = true
+    },
+    // 修改角色信息
+    editRoleInfo () {
+      this.$refs.editFormRef.validate(async valid => {
+        if (!valid) {
+          return false
+        }
+        const { data: res } = await this.$http.put('roles/' + this.editForm.roleId, {
+          roleName: this.editForm.roleName,
+          roleDesc: this.editForm.roleDesc
+        })
+        if (res.meta.status !== 200) {
+          return this.$message.error('更新角色信息失败!')
+        }
+        // 关闭对话框
+        this.editRoleDialogVisible = false
+        // 刷新数据列表
+        await this.getRolesList()
+        // 提示修改成功
+        this.$message.success('更新角色信息成功！')
+      })
     }
   }
 }
