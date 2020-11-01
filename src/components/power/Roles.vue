806875c8b991deab68e9742fc2ee178d3eaa920d
tung Leung <tungleung@MacBook-Pro.local>
2020-11-01 16:22:37 +0800

    完成角色列表删除功能

--- src/components/power/Roles.vue
+++ src/components/power/Roles.vue
@@ -50,7 +50,7 @@
         <el-table-column label="操作">
           <template slot-scope="scope">
             <el-button size="mini" type="primary" icon="el-icon-edit" @click="showEditRoleDialog(scope.row.id)">编辑</el-button>
-            <el-button size="mini" type="danger" icon="el-icon-delete">删除</el-button>
+            <el-button size="mini" type="danger" icon="el-icon-delete" @click="removeRoleById(scope.row.id)">删除</el-button>
             <el-button size="mini" type="warning" icon="el-icon-setting" @click="showSetRightDialog(scope.row)">分配权限</el-button>
           </template>
         </el-table-column>
@@ -299,6 +299,26 @@ export default {
         // 提示修改成功
         this.$message.success('更新角色信息成功！')
       })
+    },
+    // 根据id删除对应的角色信息
+    async removeRoleById (id) {
+      const confirmresult = await this.$confirm('此操作将永久删除该角色, 是否继续?', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).catch(err => err)
+      // 如果用户确认删除，则返回值为字符串 confirm
+      // 如果用户取消删除，则返回值为字符串 cancel
+      if (confirmresult !== 'confirm') {
+        return this.$message.info('已取消删除！')
+      }
+      console.log('确认删除！')
+      const { data: res } = await this.$http.delete('roles/' + id)
+      if (res.meta.status !== 200) {
+        return this.$message.error('删除角色失败！')
+      }
+      this.$message.success('删除角色成功！')
+      await this.getRolesList()
     }
   }
 }
