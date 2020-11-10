afc806467a718e3ab46ed5c639cfb20e96e2649f
梁通 <liangtong@liangtongdeMacBook-Pro.local>
2020-11-11 00:41:59 +0800

    商品添加功能开发中

--- src/main-dev.js
+++ src/main-dev.js
@@ -36,6 +36,16 @@ Vue.config.productionTip = false
 
 Vue.use(ElementUI)
 
+Vue.filter('dateFormat', function (originValue) {
+  const dt = new Date(originValue)
+  const y = dt.getFullYear()
+  const m = (dt.getMonth() + 1 + '').padStart(2, '0')
+  const d = (dt.getDate() + '').padStart(2, '0')
+  const hh = (dt.getHours() + '').padStart(2, '0')
+  const mm = (dt.getMinutes() + '').padStart(2, '0')
+  const ss = (dt.getSeconds() + '').padStart(2, '0')
+  return `${y}-${m}-${d} ${hh}:${mm}:${ss}`
+})
 new Vue({
   router,
   store,
